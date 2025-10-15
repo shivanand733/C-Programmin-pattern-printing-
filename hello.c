de5f74ba6677a44639e392ef8be58ef5f33c2a1b
shivanand733 <shivanandkumbar5604@gmail.com>
2025-10-16 00:03:08 +0530

    hello world

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>  // include standard input/output library
+
+int main() {
+    printf("Hello, World!");  // prints message to screen
+    return 0;  // program ends successfully
+}
